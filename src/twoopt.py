7a06034a8ca78c180fb144ead9aa6c1243e3b713
Wilton <wiltoon>
2022-04-27 21:46:55 -0400

    testar

--- src/twoopt.py
+++ src/twoopt.py
@@ -1,10 +1,16 @@
-from audioop import reverse
+def betterTour(new_tour, best_tour, matrix_distance):
+    new_distance = 0
+    best_distance = 0
+    for i in range(len(best_tour)-1):
+        best_distance += matrix_distance[best_tour[i]][best_tour[i+1]]
+        new_distance += matrix_distance[new_tour[i]][new_tour[i+1]]
+    return new_distance < best_distance
 
-def twoOpt(current_tour){
+def twoOpt(current_tour, matrix_distance):
     n = len(current_tour)
     best_tour = current_tour.copy()
-    for(int i=1; i<n-2; i = i+1){
-        for(int j = i+1; j < n+1; j = j+1){
+    for i in range(1,n-2):
+        for j in range(i+1, n):
             if (j-i) == 1:
                 continue
             else:
@@ -15,8 +21,8 @@ def twoOpt(current_tour){
                 t_vet = slice(j,n)
                 t_part = current_tour[t_vet]
                 new_tour = [*f_part, *s_part, *t_part]
-                if(betterTour(new_tour, best_tour):
+                if betterTour(new_tour, best_tour, matrix_distance):
                     best_tour = new_tour
-        }
-    }
-}
+    return best_tour
+
+def solve(current_tour, matrix_distance):
